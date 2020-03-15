2d6ce8035c8187467db7fee385021fe785011c26
shmck <shawn.j.mckay@gmail.com>
2020-03-14 18:41:24 -0700

    2.1 install moment

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,5 @@
 const {
-  promises: { readFile }
+  promises: { readFile, readdir }
 } = require("fs");
 const { join } = require("path");
 
@@ -21,3 +21,66 @@ const getPackageJson = async () => {
 };
 
 exports.getPackageJson = getPackageJson;
+
+/**
+ * isModuleInstalled
+ * @param { name, type } params
+ * "name" - the name of the dependency
+ * "type" - "dependency", "devDependency", "peerDependency"
+ * @returns boolean
+ */
+const isModuleInstalled = async ({ name, type }) => {
+  // 1. load package.json file
+  const json = await getPackageJson();
+
+  // 2. verify package lists dependency by type
+  let installCommand;
+  let hasDependency;
+
+  switch (type) {
+    case "dependency":
+      installCommand = "--save";
+      hasDependency = !!json.dependencies && json.dependencies[name];
+      break;
+    case "devDependency":
+      installCommand = "--save-dev";
+      hasDependency = !!json.devDependencies && json.devDependencies[name];
+      break;
+    case "peerDependency":
+      throw new Error("Peer dependencies unsupported");
+    default:
+      throw new Error("Unsupported packaged type");
+  }
+
+  if (!hasDependency) {
+    throw new Error(`Run "npm install ${installCommand} ${name}"`);
+  }
+
+  // 3. verify node_module installed
+  const pathToNodeModule = join(process.cwd(), "node_modules", name);
+  const hasNodeModules = await readdir(pathToNodeModule).catch(() => {
+    throw new Error('Missing node_modules. Run "npm install"');
+  });
+  if (!hasNodeModules) {
+    throw new Error('Missing node_modules. Run "npm install"');
+  }
+
+  // 4. is installed
+  return true;
+};
+
+exports.isModuleInstalled = isModuleInstalled;
+
+// created because assert.doesNotThrow not working predictably with async fns
+const doesNotThrow = async fn => {
+  let result = true;
+  try {
+    await fn();
+  } catch (error) {
+    console.error(error);
+    result = false;
+  }
+  return result;
+};
+
+exports.doesNotThrow = doesNotThrow;
